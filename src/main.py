77fb973c8f47321db81e433b89d655d47012fd85
AdenSial <adensyal2003@gmail.com>
2025-11-26 09:27:53 +0500

    feat: add video streams router and update CORS config

--- src/main.py
+++ src/main.py
@@ -104,12 +104,16 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "http://localhost:5174",
+        "http://127.0.0.1:5174",
         "http://localhost:3000",
         "http://127.0.0.1:3000"
     ],
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     allow_headers=["*"],
+    expose_headers=["*"],
+    max_age=3600,
 )
 
 
